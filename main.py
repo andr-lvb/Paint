d5ebce33ab57464a941cc512efc35febb71abf83
andr-lvb <andreylamtyuhov@gmail.com>
2026-07-03 22:07:16 +0300

    '3'

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import const as cn
-from objects import Canvas, Palette, Info_Panel, Circle, Button
+from objects import Canvas, Palette, Info_Panel, Circle, Button, Vector2D, Point2D
 from draw import draw_line_dda
 
 # инициализируем pygame
@@ -18,8 +18,11 @@ info_panel = Info_Panel(screen, 800, 80)
 # создаем меню palette
 palette = Palette(info_panel, cn.PALETTE_COLORS, 800, 80)
 
-# создаем ластик
-eraser_button = Button(240, 20, 40, 40, cn.GRAY)
+# создаем конпку ластика
+eraser_button = Button(240, 10, 60, 60, cn.GRAY)
+
+# создаем конпку для измнения линии
+edit_button = Button(350, 10, 60, 60, cn.BLUE)
 
 # устанавливаем название для игры
 pg.display.set_caption('Paint')
@@ -30,8 +33,12 @@ clock = pg.time.Clock()
 running = True
 point = 0
 list_point = []
+lines = []
+select_line_index = -1
+edit_mode = False
+drag_end = 0
 eraser_mode = False
-eraser_radius = 20
+eraser_radius = 2
 
 def create_eraser_cursor(radius, cell_size):
     size = radius * 2 * cell_size
@@ -40,6 +47,7 @@ def create_eraser_cursor(radius, cell_size):
     pg.draw.circle(surface, (255, 0, 0, 100), (size//2, size//2), radius*cell_size, 1)
     pg.draw.circle(surface, (255, 0, 0, 30), (size//2, size//2), radius*cell_size)
     return pg.cursors.Cursor((size//2, size//2), surface)
+
 
 while running:
     for event in pg.event.get():
@@ -62,10 +70,23 @@ while running:
                     eraser_mode = not eraser_mode
                     eraser_button.active = eraser_mode
 
+                if edit_button.is_clicked(p_info):
+                    edit_mode = not edit_mode
+                    edit_button.active = edit_mode
+
+                    if edit_mode:
+                        eraser_mode = False
+                        eraser_button.active = False
+                        point = 0
+                        list_point = []
+                        pg.mouse.set_cursor(pg.cursors.arrow)
+                    selected_line_index = -1
+
                     if eraser_mode:
                         point = 0
                         list_point = []
                         pg.mouse.set_cursor(create_eraser_cursor(eraser_radius, cn.CELL_SIZE))
+
 
                     else:
                         pg.mouse.set_cursor(pg.cursors.arrow)
@@ -85,10 +106,10 @@ while running:
 
                     if not point:
                         if event.button == 1:
-                            canvas.set_pixel(x, y, cn.RED)
+                            canvas.set_pixel(x, y, palette.current_color)
 
                         elif event.button == 2:
-                            circle = Circle(p, 200, cn.RED)
+                            circle = Circle(p, 20, palette.current_color)
                             circle.draw(canvas)
 
                         elif event.button == 3:
@@ -99,13 +120,19 @@ while running:
                             list_point.append(p)
                             point -= 1
                             if point == 0:
-                                draw_line_dda(canvas, list_point[0], list_point[1], cn.GREEN)
+                                draw_line_dda(canvas, list_point[0], list_point[1], palette.current_color)
+                                lines.append((list_point[0], list_point[1]))
                                 list_point = []
 
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_r:
-                        point = 2
-                        list_point = []
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_r:
+                point = 2
+                list_point = []
+            if event.key == pg.K_t:
+                p1 = Point2D(4, 5)
+                v = Vector2D.from_angle(10, -45)
+                p2 = p1.move_by_vector(v)
+                draw_line_dda(canvas, p1, p2, palette.current_color)
 
         if event.type == pg.MOUSEMOTION and eraser_mode:
             if event.buttons[0] == 1:
@@ -117,11 +144,11 @@ while running:
                             canvas.set_pixel(cx + dx, cy + dy, canvas.bg_color)
 
 
-    # отрисовываем все поверхности
+    # отрисовываем все поверхности и кнопки
     canvas.draw()
     info_panel.draw()
     palette.draw()
-
+    edit_button.draw(info_panel)
     eraser_button.draw(info_panel)
 
     # обновление экрана (что бы отображалось нарисованное)
--- objects.py
+++ objects.py
@@ -1,6 +1,157 @@
 import pygame as pg
 import const as cn
 from draw import draw_line_dda
+import math
+
+
+
+
+class Panel:
+    def __init__(self, screen, width, height, bg_color=cn.WHITE):
+        self.width = width
+        self.height = height
+        self.screen = screen
+        self.pixels = []
+        self.bg_color = bg_color
+        for r in range(self.height):
+            row = []
+            for c in range(self.width):
+                row.append(self.bg_color)
+            self.pixels.append(row)
+
+    def get_coords(self):
+        pass
+
+    def get_pixel(self, x, y):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            return self.pixels[y][x]
+        return None
+
+    def set_pixel(self, x, y, color=None):
+        if 0 <= x < self.width and 0 <= y < self.height:
+            self.pixels[y][x] = color if color else self.bg_color
+
+    def draw(self):
+        pass
+
+    def fill_rect(self, rect, color):
+        for r in range(rect.height):
+            for c in range(rect.width):
+                self.set_pixel(rect.x + c, rect.y + r, color)
+
+
+
+
+class Canvas(Panel):
+    def __init__(self, screen, width, height, bg_color=cn.WHITE):
+        super().__init__(screen, width, height, bg_color)
+
+    def get_coords(self):
+        x, y = pg.mouse.get_pos()
+        canvas_x = x // cn.CELL_SIZE
+        canvas_y = (y - 80) // cn.CELL_SIZE
+        return Point2D(canvas_x, canvas_y)
+
+    def draw(self):
+        for x in range(self.height):
+            for y in range(self.width):
+                color = self.pixels[y][x]
+                rect = pg.Rect(x * cn.CELL_SIZE, y * cn.CELL_SIZE + 80, cn.CELL_SIZE, cn.CELL_SIZE)
+
+                pg.draw.rect(self.screen, color, rect)
+                if cn.CELL_SIZE >= 3:
+                    pg.draw.rect(self.screen, cn.COLOR_LINE_BETWEEN_CELLS, rect, 1)
+
+
+
+
+class Info_Panel(Panel):
+    def __init__(self, screen, width, height, bg_color=cn.BLACK):
+        super().__init__(screen, width, height, bg_color)
+
+    def draw(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                color = self.pixels[y][x]
+                rect = pg.Rect(x, y, 1, 1)
+                pg.draw.rect(self.screen, color, rect)
+
+    def get_coords(self):
+        x, y = pg.mouse.get_pos()
+        return Point2D(x, y)
+
+
+
+
+class Palette:
+    def __init__(self, info_panel, colors, width, height, cell_size=30, margin=5):
+        self.info_panel = info_panel
+        self.colors = colors
+        self.width = width
+        self.height = height
+        self.cell_size = cell_size
+        self.margin = margin
+        self.rects = []
+        self.current_color = colors[2]
+
+    def draw(self):
+        cols = 4
+        rows = 2
+        start_x = self.margin
+        start_y = self.margin
+        i = 0
+        for r in range(rows):
+            for c in range(cols):
+                x = start_x + c * (self.cell_size + self.margin)
+                y = start_y + r * (self.cell_size + self.margin)
+                rect = Rectangle(x, y, self.cell_size, self.cell_size, self.colors[i])
+                rect.draw(self.info_panel)
+                self.info_panel.fill_rect(rect, self.colors[i])
+                i += 1
+        rect = Rectangle(170, 25, self.cell_size, self.cell_size, self.current_color)
+        rect.draw(self.info_panel)
+        self.info_panel.fill_rect(rect, self.current_color)
+
+    def get_color(self, p):
+        x, y = p.conv()
+        if y > 80:
+            return self.current_color
+        if self.margin <= x <= self.cell_size * 4 + self.margin * 4:
+            col = int((x - self.margin / 2) // (self.margin + self.cell_size))
+            row = int((y - self.margin / 2) // (self.margin + self.cell_size))
+            ix = x - col * ( self.margin + self.cell_size) - self.margin / 2
+            iy = y - row * ( self.margin + self.cell_size) - self.margin / 2
+            if self.margin / 2 <= ix <= self.cell_size and self.margin / 2 <= iy <= self.cell_size:
+                return self.colors[4 * row + col]
+        return self.current_color
+
+
+
+
+class Button:
+    def __init__(self, x, y, width, height, color, text=''):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.text = text
+        self.active = False
+        self.rect = Rectangle(x, y, self.width, self.height, color)
+
+    def draw(self, surface):
+        surface.fill_rect(self.rect, self.color)
+        if self.active:
+            x, y = self.x, self.y
+            width, height = self.width - 1, self.height - 1
+            draw_line_dda(surface, Point2D(x, y), Point2D(x + width, y), cn.GREEN, antialiasing=False)
+            draw_line_dda(surface, Point2D(x, y + height), Point2D(x + width, y + height), cn.GREEN, antialiasing=False)
+            draw_line_dda(surface, Point2D(x, y), Point2D(x, y + height), cn.GREEN, antialiasing=False)
+            draw_line_dda(surface, Point2D(x + width, y), Point2D(x + width, y + height), cn.GREEN, antialiasing=False)
+
+    def is_clicked(self, point):
+        px, py = point.conv()
+        return self.x <= px < self.x + self.width and self.y <= py < self.y + self.height
 
 
 
@@ -21,6 +172,9 @@ class Point2D(Point):
 
     def conv(self):
         return self.x, self.y
+
+    def move_by_vector(self, vector):
+        return Point2D(self.x + vector.dx, self.y - vector.dy)
 
 
 
@@ -53,7 +207,7 @@ class Polygon2D(Shape2D):
         for i in range(len(self.points)):
             p1 = self.points[i]
             p2 = self.points[(i + 1) % len(self.points)]
-            draw_line_dda(surface, p1, p2, self.color)
+            draw_line_dda(surface, p1, p2, self.color, antialiasing=False)
 
 
 
@@ -117,142 +271,42 @@ class Square(Rhombus):
     def __init__(self, center, size,  color):
         super().__init__(center, size, size, color)
 
-class Panel:
-    def __init__(self, screen, width, height, bg_color=cn.WHITE):
-        self.width = width
-        self.height = height
-        self.screen = screen
-        self.pixels = []
-        self.bg_color = bg_color
-        for r in range(self.height):
-            row = []
-            for c in range(self.width):
-                row.append(self.bg_color)
-            self.pixels.append(row)
-
-    def get_coords(self):
-        pass
-
-    def get_pixel(self, x, y):
-        if 0 <= x < self.width and 0 <= y < self.height:
-            return self.pixels[y][x]
-        return None
-
-    def set_pixel(self, x, y, color=None):
-        if 0 <= x < self.width and 0 <= y < self.height:
-            self.pixels[y][x] = color if color else self.bg_color
-
-    def draw(self):
-        pass
-
-    def fill_rect(self, rect, color):
-        for r in range(rect.height):
-            draw_line_dda(self, Point2D(rect.x, rect.y + r ), Point2D(rect.x + rect.width - 1, rect.y + r), color)
-
-class Canvas(Panel):
-    def __init__(self, screen, width, height, bg_color=cn.WHITE):
-        super().__init__(screen, width, height, bg_color)
-
-    def get_coords(self):
-        x, y = pg.mouse.get_pos()
-        canvas_x = x // cn.CELL_SIZE
-        canvas_y = (y - 80) // cn.CELL_SIZE
-        return Point2D(canvas_x, canvas_y)
-
-    def draw(self):
-        for x in range(self.height):
-            for y in range(self.width):
-                color = self.pixels[y][x]
-                rect = pg.Rect(x * cn.CELL_SIZE, y * cn.CELL_SIZE + 80, cn.CELL_SIZE, cn.CELL_SIZE)
-
-                pg.draw.rect(self.screen, color, rect)
-                if cn.CELL_SIZE >= 3:
-                    pg.draw.rect(self.screen, cn.COLOR_LINE_BETWEEN_CELLS, rect, 1)
-
-class Info_Panel(Panel):
-    def __init__(self, screen, width, height, bg_color=cn.BLACK):
-        super().__init__(screen, width, height, bg_color)
-
-    def draw(self):
-        for x in range(self.width):
-            for y in range(self.height):
-                color = self.pixels[y][x]
-                rect = pg.Rect(x, y, 1, 1)
-                pg.draw.rect(self.screen, color, rect)
-
-    def get_coords(self):
-        x, y = pg.mouse.get_pos()
-        return Point2D(x, y)
-
-class Palette:
-    def __init__(self, info_panel, colors, width, height, cell_size=30, margin=5):
-        self.info_panel = info_panel
-        self.colors = colors
-        self.width = width
-        self.height = height
-        self.cell_size = cell_size
-        self.margin = margin
-        self.rects = []
-        self.current_color = colors[2]
-
-    def draw(self):
-        cols = 4
-        rows = 2
-        start_x = self.margin
-        start_y = self.margin
-        i = 0
-        for r in range(rows):
-            for c in range(cols):
-                x = start_x + c * (self.cell_size + self.margin)
-                y = start_y + r * (self.cell_size + self.margin)
-                rect = Rectangle(x, y, self.cell_size, self.cell_size, self.colors[i])
-                rect.draw(self.info_panel)
-                self.info_panel.fill_rect(rect, self.colors[i])
-                i += 1
-        rect = Rectangle(170, 25, self.cell_size, self.cell_size, self.current_color)
-        rect.draw(self.info_panel)
-        self.info_panel.fill_rect(rect, self.current_color)
-
-    def get_color(self, p):
-        x, y = p.conv()
-        if y > 80:
-            return self.current_color
-        if self.margin <= x <= self.cell_size * 4 + self.margin * 4:
-            col = int((x - self.margin / 2) // (self.margin + self.cell_size))
-            row = int((y - self.margin / 2) // (self.margin + self.cell_size))
-            ix = x - col * ( self.margin + self.cell_size) - self.margin / 2
-            iy = y - row * ( self.margin + self.cell_size) - self.margin / 2
-            if self.margin / 2 <= ix <= self.cell_size and self.margin / 2 <= iy <= self.cell_size:
-                return self.colors[4 * row + col]
-            else:
-                return self.current_color
 
 
 
+class Vector:
+    pass
 
 
-class Button:
-    def __init__(self, x, y, width, height, color, text=''):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-        self.text = text
-        self.active = False
-        self.rect = Rectangle(x, y, self.width, self.height, color)
 
-    def draw(self, surface):
-        surface.fill_rect(self.rect, self.color)
-        if self.active:
-            x, y = self.x, self.y
-            width, height = self.width - 1, self.height - 1
-            draw_line_dda(surface, Point2D(x, y), Point2D(x + width, y), cn.GREEN)
-            draw_line_dda(surface, Point2D(x, y + height), Point2D(x + width, y + height), cn.GREEN)
-            draw_line_dda(surface, Point2D(x, y), Point2D(x, y + height), cn.GREEN)
-            draw_line_dda(surface, Point2D(x + width, y), Point2D(x + width, y + height), cn.GREEN)
+class Vector2D(Vector):
+    def __init__(self, dx, dy):
+        self.dx = dx
+        self.dy = dy
 
-    def is_clicked(self, point):
-        px, py = point.conv()
-        return self.x <= px < self.x + self.width and self.y <= py < self.y + self.height
+    @classmethod
+    def from_angle(cls, len, digree):
+        r = math.radians(digree)
+        dx = len * math.cos(r)
+        dy = len * math.sin(r)
+        return cls(dx, dy)
+
+    def __str__(self):
+        return f"({self.dx}, {self.dy})"
+
+    @property
+    def length(self):
+        return (self.dx ** 2 + self.dy ** 2) ** 0.5
+
+    @property
+    def angle(self):
+        return math.degrees(math.atan2(self.dy, self.dx))
+
+    def __add__(self, other):
+        return Vector2D(self.dx + other.dx, self.dy + other.dy)
+
+    def __sub__(self, other):
+        return Vector2D(self.dx - other.dx, self.dy - other.dy)
+
+
 
